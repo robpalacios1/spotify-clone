45b694a99a85464c1f68771109af5a6b0cff5895
roberto <robpalacios1@hotmail.com>
2021-01-12 11:51:33 -0500

    add Login.jsx component

---
+++ src/components/Login.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+/***** CSS *****/
+import './Login.css'
+
+const Login = () => {
+    return (
+        <div className="login">
+            <img
+                src="https://getheavy.com/wp-content/uploads/2019/12/spotify2019-830x350.jpg"
+                alt=""
+            />
+            {/** Login with spotify button */}
+            <a href>LOGIN WITH SPOTIFY</a>
+        </div>
+    )
+}
+
+export default Login
