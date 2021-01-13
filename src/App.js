9ff6cc447044e861b4f87bbd7d74a41a2432b5ff
roberto <robpalacios1@hotmail.com>
2021-01-13 11:15:46 -0500

    add useEffect

--- src/App.js
+++ src/App.js
@@ -1,12 +1,22 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 
 /***** Components *****/
 import Login from './components/Login'
 
+/***** Spotify *****/
+import { getTokenFromUrl } from './spotify'
+
 /***** CSS *****/
 import './App.css';
 
+
 function App() {
+
+  useEffect(() => {
+    const token = getTokenFromUrl();
+    console.log(token)
+  }, [])
+
   return (
     <div className="App">
       <Login />
